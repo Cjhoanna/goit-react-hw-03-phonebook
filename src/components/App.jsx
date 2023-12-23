597c2058ae425182381dd0f6176d367f0b2ae0fc
Cjhoanna <pacachaux46@gmail.com>
2023-12-22 21:58:20 -0500

    finish component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,6 +14,22 @@ class App extends Component {
     filter: '',
   };
 
+  componentDidMount(){
+    const savedContacts= localStorage.getItem("contacts")
+    if (savedContacts) {
+      this.setState({
+        contacts:JSON.parse(savedContacts)
+      })
+    }
+  }
+
+  componentDidUpdate(prevProps,prevState){
+    if (prevState.contacts!==this.state.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts))
+    }
+    
+  }
+
   submitHandle = event => {
     event.preventDefault();
     const form = event.target;
